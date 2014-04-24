b0f2bf33424a605970437d4efce6413a59284997
John Back <johndavidback@gmail.com>
2014-04-24 15:36:17 -0400

    Adding in Facebook login, next step: connect it up

--- kyle.py
+++ kyle.py
@@ -3,10 +3,12 @@ app = Flask(__name__)
 
 app.debug = True
 
+from local_config import FACEBOOK_APP_ID
+
 @app.route('/')
 def home():
 
-    return render_template('home.html')
+    return render_template('home.html', fb_app_id=FACEBOOK_APP_ID)
 
 
 if __name__ == '__main__':
